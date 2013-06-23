8a1a1f0991d8f7bc6b323f2919b5288b6b4809d8
mani <mani95lisa@gmail.com>
2013-06-23 22:42:02 +0800

    update redis src

--- installRedis.sh
+++ installRedis.sh
@@ -5,7 +5,7 @@ echo "Going to install Redis"
 
 wget http://redis.googlecode.com/files/redis-2.6.13.tar.gz
 tar xzf redis-2.6.13.tar.gz
-cd redis-2.6.13
+cd redis-2.6.13/src
 make
  
 mkdir -p /usr/local/bin
