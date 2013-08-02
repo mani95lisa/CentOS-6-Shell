d43b29d1c3b579b6095edee6d746a39399c9637b
mani <mani95lisa@gmail.com>
2013-08-02 14:11:21 +0800

    update rpm variable

--- installApacheBenchmark.sh
+++ installApacheBenchmark.sh
@@ -9,7 +9,7 @@ cd ~/http
 yumdownloader httpd
 read -p 'Enter download rpm : (httpd-2.2.15-15.el6.centos.1.x86_64.rpm) ' rpm
 rpm = ${rpm:httpd-2.2.15-15.el6.centos.1.x86_64.rpm}
-rpm2cpio rpm | cpio -idmv
+rpm2cpio $rpm | cpio -idmv
 mv usr/bin/ab /usr/bin/ab
 cd ~
 rm -rf ~/httpd
