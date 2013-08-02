52f357770eca6fcbdab3df504cae42bc2deb2780
mani <mani95lisa@gmail.com>
2013-08-02 14:15:58 +0800

    use install httpd-tools

--- installApacheBenchmark.sh
+++ installApacheBenchmark.sh
@@ -2,17 +2,7 @@
 
 clear
 echo "Go to install Apache Benchmark"
-yum -y install apr-util
-yum -y install yum-utils
-mkdir ~/http
-cd ~/http
-yumdownloader httpd
-read -p 'Enter download rpm : (httpd-2.2.15-15.el6.centos.1.x86_64.rpm) ' rpm
-rpm = ${rpm:-httpd-2.2.15-15.el6.centos.1.x86_64.rpm}
-rpm2cpio $rpm | cpio -idmv
-mv usr/bin/ab /usr/bin/ab
-cd ~
-rm -rf ~/httpd
+sudo yum install httpd-tools
 
 echo "Run ab"
 ab http://www.google.com
